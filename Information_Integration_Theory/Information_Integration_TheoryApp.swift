49410a7cbd14d8f2bea0d3efae20afd3a6c0926c
Cong Le <longchike@gmail.com>
2025-06-29 22:30:57 -0700

    The setup

---
+++ Information_Integration_Theory/Information_Integration_TheoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Information_Integration_TheoryApp.swift
+//  Information_Integration_Theory
+//
+//  Created by Cong Le on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Information_Integration_TheoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
